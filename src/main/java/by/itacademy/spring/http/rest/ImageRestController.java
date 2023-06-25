12fa6f0c28810f84352f76f91aab5f38de6412a0
Ahmed <83308467+Guardian-lang@users.noreply.github.com>
2023-06-25 18:33:51 +0300

    SpringHomework

--- src/main/java/by/itacademy/spring/http/rest/ImageRestController.java
+++ src/main/java/by/itacademy/spring/http/rest/ImageRestController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
